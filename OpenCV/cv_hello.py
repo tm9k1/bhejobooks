cf46608742c6b66da81b2113a36fca1cd4b7469d
brute4s99 <piyushaggarwal002@gmail.com>
2018-01-31 05:06:47 +0530

    python cv practices

---
+++ OpenCV/cv_hello.py
@@ -0,0 +1,13 @@
+import cv2
+import time
+import os
+cwd = os.getcwd()
+# print cwd
+img=cv2.imread("test.jpg",1)
+img2=cv2.imread('testwrite.jpg',1)
+for i in range(101):
+	cv2.imshow('testing',cv2.addWeighted(img2,float(i)/100,img,1-(float(i)/100),0))
+	cv2.waitKey(40)
+cv2.imshow('testing',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
